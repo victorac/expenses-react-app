874809bca61a0aea34f474a4147f9c60f060ce62
victorac <victorde.ac@gmail.com>
2023-02-02 19:35:22 -0300

    refactor state to keep the selected year instead of the filtered expenses; add treat the select onChange event inside ExpenseFilter

--- src/components/expenses/ExpenseFilter.js
+++ src/components/expenses/ExpenseFilter.js
@@ -1,11 +1,15 @@
 import Card from "../ui/Card";
 
-function ExpenseFilter({ expenses, onSelectYear }) {
+function ExpenseFilter({ expenses, year, onSelectYear }) {
+  const selectHandler = (event) => {
+    onSelectYear(event.target.value);
+  };
+
   let totalAmount = 0;
   if (expenses.length > 0) {
-    totalAmount = expenses.map(el => el.amount).reduce(
-      (prev, curr) => Number(curr) + Number(prev)
-    );
+    totalAmount = expenses
+      .map((el) => el.amount)
+      .reduce((prev, curr) => Number(curr) + Number(prev));
   }
   console.log(expenses);
   const monthData = [
@@ -23,15 +27,16 @@ function ExpenseFilter({ expenses, onSelectYear }) {
     { percentage: 0, month: "Dec" },
   ];
   expenses.forEach((element) => {
-    monthData[element.date.getMonth()].percentage += Number(element.amount) / totalAmount * 100;
+    monthData[element.date.getMonth()].percentage +=
+      (Number(element.amount) / totalAmount) * 100;
   });
-  console.log(monthData);
+
   return (
     <div className="flex flex-col gap-4 w-[50rem] max-w-[95%] mx-auto">
       <div className="flex flex-row p-5">
         <label className="flex flex-1">Filter by year</label>
-        <select onChange={onSelectYear}>
-        <option value="All">All</option>
+        <select value={year} onChange={selectHandler}>
+          <option value="All">All</option>
           <option value="2023">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
@@ -40,7 +45,13 @@ function ExpenseFilter({ expenses, onSelectYear }) {
         </select>
       </div>
       <Card className="flex flex-row gap-4 text-sm bg-orange-200 px-3 py-2">
-        {monthData.map(data => <MonthBar key={data.month} month={data.month} percentage={data.percentage}/>)}
+        {monthData.map((data) => (
+          <MonthBar
+            key={data.month}
+            month={data.month}
+            percentage={data.percentage}
+          />
+        ))}
       </Card>
     </div>
   );
@@ -52,10 +63,7 @@ function MonthBar({ month, percentage }) {
   };
   return (
     <div className="flex flex-1 flex-col items-center gap-1">
-      <div
-        className="h-[100px] w-4 rounded-md"
-        style={backgrounColor}
-      ></div>
+      <div className="h-[100px] w-4 rounded-md" style={backgrounColor}></div>
       <span>{month}</span>
     </div>
   );
--- src/components/expenses/ExpenseList.js
+++ src/components/expenses/ExpenseList.js
@@ -5,25 +5,18 @@ import ExpenseFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 
 function ExpenseList({ expenses }) {
-  const [filteredExpenses, setFilteredExpenses] = useState(expenses);
+  const [selectedYear, setSelectedYear] = useState("2023");
+  const filteredExpenses = expenses.filter(
+    (expense) => String(expense.date.getFullYear()) === selectedYear || selectedYear === "All"
+  );
 
-  const onSelectYear = (event) => {
-    const selectedYear = event.target.value;
-    let filtered;
-    if (selectedYear === "All") {
-      filtered = expenses;
-    } else {
-      filtered = expenses.filter(
-        (expense) => String(expense.date.getFullYear()) === selectedYear
-      );
-    }
-
-    setFilteredExpenses(filtered);
+  const onSelectYear = (year) => {
+    setSelectedYear(year);
   };
 
   return (
     <Card className="p-5 flex-col gap-2 bg-indigo-200 my-2 mx-auto w-[50rem] max-w-[95%]">
-      <ExpenseFilter expenses={filteredExpenses} onSelectYear={onSelectYear} />
+      <ExpenseFilter expenses={filteredExpenses} onSelectYear={onSelectYear} year={selectedYear} />
       {filteredExpenses.map((expense, index) => (
         <ExpenseItem
           key={`expense-${index}`}
